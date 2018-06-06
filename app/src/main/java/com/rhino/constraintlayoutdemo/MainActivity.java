3c069cbeb5c0c81cfaa0c124e2e00d05c66ee44b
luolin <rhino_luo@163.com>
2018-06-06 11:10:02 +0800

    Add demo for chains.

--- app/src/main/java/com/rhino/constraintlayoutdemo/MainActivity.java
+++ app/src/main/java/com/rhino/constraintlayoutdemo/MainActivity.java
@@ -3,11 +3,16 @@ package com.rhino.constraintlayoutdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+/**
+ * @author LuoLin
+ * @since Create on 2018/6/5.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.chains);
     }
 }
